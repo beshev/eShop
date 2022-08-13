bbcd665c6ff335b80ee4540cafbc5a125bda7357
beshev <beshev1@gmail.com>
2022-08-13 08:39:46 +0300

    Constatnt for product category name added

--- src/Data/EShop.Data.Models/ProductCategory.cs
+++ src/Data/EShop.Data.Models/ProductCategory.cs
@@ -3,6 +3,7 @@
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
+    using EShop.Common;
     using EShop.Data.Common.Models;
 
     public class ProductCategory : BaseModel<int>
@@ -13,7 +14,7 @@
         }
 
         [Required]
-        [MaxLength(255)]
+        [MaxLength(DataConstants.ProductCategoryNameMaxLength)]
         public string Name { get; set; }
 
         public IEnumerable<Product> Products { get; set; }
--- src/EShop.Common/DataConstants.cs
+++ src/EShop.Common/DataConstants.cs
@@ -5,6 +5,8 @@
         // Product entity
         public const int ProductNameMaxLength = 255;
 
+        public const int ProductCategoryNameMaxLength = 255;
+
         // Template entity
         public const int TemplateNameMaxLength = 255;
     }
