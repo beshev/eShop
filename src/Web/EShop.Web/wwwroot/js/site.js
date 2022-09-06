c4c7413d7e8ff451d39c7df164ab24e42a8c53cb
beshev <beshev1@gmail.com>
2022-09-06 20:14:20 +0300

    work on front-end

--- src/Web/EShop.Web/wwwroot/js/site.js
+++ src/Web/EShop.Web/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function isValid() {
+    let inputElemets = [...document.querySelectorAll("form input")];
+    let isNotValid = inputElemets.some(x => x.value === '')
+
+    var button = document.getElementById("submitButton");
+
+    if (isNotValid) {
+        button.setAttribute('disabled', 'disabled');
+    }
+    else {
+        button.removeAttribute('disabled');
+    }
+}
